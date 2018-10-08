8d5f605fbe51e0c5bbef57ef6c758aa1c222a2af
pmb <gambler1541@gmail.com>
2018-10-08 17:08:09 +0900

    mb-project first commit

---
+++ app/config/settings/local.py
@@ -0,0 +1,19 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
