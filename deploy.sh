4c2613b73cdf1b642bfb0fce73ad249f45c94057
pmb <gambler1541@gmail.com>
2018-10-17 12:11:25 +0900

    deploy.sh 작성

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+pipenv  lock --requirements > requirements.txt
+git     add -f .secrets
+git     add -f requirements.txt
+git     status
+eb      deploy --profile project-pmb --staged
+git     reset head .secrets
+git     reset head requirements.txt
+rm      requirements.txt
