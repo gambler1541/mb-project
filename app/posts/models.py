75e99403925564462513c4678883325c714a1df9
pmb <gambler1541@gmail.com>
2018-12-03 10:20:45 +0900

    comment가 저장 될때 tag를 db에 저장

--- app/posts/models.py
+++ app/posts/models.py
@@ -49,35 +49,50 @@ class Comment(models.Model):
         verbose_name='해시태그 목록',
     )
 
+    # Comment의 save()가 호출 될 때,
+    # content의 값을 사용해서 이 필드가 자동으로 채운 후 저장하기
+    _html = models.TextField('태그가 HTML화된 댓글 내용', blank=True,)
+
     class Meta:
         verbose_name = '댓글'
         verbose_name_plural = f'{verbose_name} 목록'
 
     def save(self, *args, **kwargs):
-        # DB에 변경내역ㅇ르 기록한 상태
-        super().save(*args, **kwargs)
-
-        # DB에 Comment저장이 완료된 후,
-        #   자신의 'content'값에서 해시태그 목록을 가져와서
-        #   자신의 'tags'속성 (MTM필드)에 할당
-        tags = [HashTag.objects.get_or_create(name=name)[0]
-                for name in re.findall(self.TAG_PATTERN, self.content)]
-        self.tags.set(tags)
-
-    @property
-    def html(self):
         # 자신의 content속성값에서
         # "#태그명"에 해당하는 문자열을
         #  아래와 같이 변경
         # <a href="/explore/tags/{태그명}/">{태그명}</a>
         # re.sub를 사용
+        def save_html():
+            self._html = re.sub(
+                self.TAG_PATTERN,
+                r'<a href="/explore/tags/\g<tag>/">#\g<tag></a>',
+                self.content,
+            )
 
+        def save_tags():
+            '''
+            DB에 Comment저장이 완료된 후,
+           자신의 'content'값에서 해시태그 목록을 가져와서
+           자신의 'tags'속성 (MTM필드)에 할당
+
+            '''
+            tags = [HashTag.objects.get_or_create(name=name)[0]
+                    for name in re.findall(self.TAG_PATTERN, self.content)]
+            self.tags.set(tags)
+
+
+        save_html()
+        # DB에 변경내역을 기록한 상태
+        super().save(*args, **kwargs)
+        save_tags()
+
+
+
+    @property
+    def html(self):
         # 템플릿에서는 commnet.html을 출력
-        return re.sub(
-            self.TAG_PATTERN,
-            r'<a href="/explore/tags/\g<tag>/">#\g<tag></a>',
-            self.content,
-        )
+        return self._html
 
 
 class HashTag(models.Model):
